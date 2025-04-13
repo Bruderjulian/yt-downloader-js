42fbff6ab31b7cef19ffe605bab1e719abd74bd0
BruderJulian <julian@familie-zech.de>
2025-04-13 18:05:09 +0200

    improved fetching, added compression and added ffmpeg

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ if (argv[2] === "download") {
   download(argv[3], argv[4]);
 } else if (argv[2] === "init") {
   const gh_token = process.argv[process.argv.indexOf("-t") + 1] || "";
-  const refresh = process.argv.indexOf("-r")
+  const refresh = process.argv.indexOf("-r") !== -1
   init(refresh, gh_token);
 }
--- src/init.js
+++ src/init.js
@@ -1,9 +1,11 @@
 "use strict";
 
-const { mkdir, chmod, rm } = require("node:fs/promises");
+const { chmod, rm } = require("node:fs/promises");
 const { pipeline } = require("node:stream/promises");
 const { createWriteStream, existsSync } = require("node:fs");
 const { join } = require("path");
+
+const { createGunzip } = require("zlib");
 
 let platform = process.platform.toLowerCase();
 platform =
@@ -40,7 +42,9 @@ const ffmpeg_fileName = `ffmpeg-${platform}-${process.arch}${
 const yt_dl_path = join(process.cwd(), yt_dl_fileName);
 const ffmpeg_path = join(process.cwd(), ffmpeg_fileName);
 const yt_dl_host = "https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest";
-const ffmpeg_host = `https://https://github.com/Bruderjulian/yt-downloader-js/raw/master/platforms/${platform}-${process.arch}`;
+const ffmpeg_host1 = `https://https://github.com/Bruderjulian/yt-downloader-js/raw/master/ffmpeg-patched/${platform}-${process.arch}`;
+const ffmpeg_host2 = `https://https://github.com/Bruderjulian/yt-downloader-js/raw/master/ffmpeg-unpatched/${platform}-${process.arch}`;
+
 let GITHUB_TOKEN;
 
 const getLatest = ({ assets }, name) => {
@@ -50,13 +54,8 @@ const getLatest = ({ assets }, name) => {
   return fetch(url);
 };
 
-const getBinary = async (url, name) => {
-  const headers = GITHUB_TOKEN
-    ? { Authorization: `Bearer ${GITHUB_TOKEN}` }
-    : {};
-  let response = fetch(url, headers);
-  response.catch(() => console.log("Could not fetch from: " + url + "\n"));
-  response = await response;
+const getBinary = async (url, name, fallback) => {
+  let response = await fetchRetry(url, fallback);
 
   if (response.headers.get("content-type") !== "application/octet-stream") {
     const payload = await response.json();
@@ -73,32 +72,32 @@ async function install_YTDL() {
   console.log("writing binary to: " + yt_dl_path);
   await pipeline(binary, createWriteStream(yt_dl_path));
   await chmod(yt_dl_path, 0o755);
-  console.log("Successfully installed!");
+  console.log("Successfully installed!\n");
 }
 
 async function install_FFMPEG() {
-  console.log("installing ffmpeg from: " + ffmpeg_host);
+  console.log("installing ffmpeg from: " + ffmpeg_host1);
   const [binary] = await Promise.all([
-    getBinary(ffmpeg_host, ffmpeg_fileName),
+    getBinary(ffmpeg_host1, ffmpeg_fileName, ffmpeg_host2),
     //mkdir(ffmpeg_path, { recursive: true }),
   ]);
   console.log("writing binaries to: " + ffmpeg_path);
-  await pipeline(binary, createWriteStream(ffmpeg_fileName));
-  //await chmod(ffmpeg_path, 0o755);
-  console.log("Successfully installed!");
+  await pipeline(binary, createGunzip(), createWriteStream(ffmpeg_path));
+  await chmod(ffmpeg_path, 0o755);
+  console.log("Successfully installed!\n");
 }
 
 async function init(refresh = false, gh_token) {
   GITHUB_TOKEN = gh_token;
   try {
-    let exists = existsSync(yt_dl_path);
+    var exists = existsSync(yt_dl_path);
     if (exists && refresh) {
       console.log("Removing yt-dl");
       await rm(yt_dl_path, { retryDelay: 2 });
       await install_YTDL();
     } else if (!exists) {
       await install_YTDL();
-    } else console.log("Skipping. Has already installed yt-dl!");
+    } else console.log("Skipping yt-dl. Has already installed yt-dl!\n");
   } catch (err) {
     console.error("Could not install yt-dlp due: " + err);
   }
@@ -111,12 +110,37 @@ async function init(refresh = false, gh_token) {
       await install_FFMPEG();
     } else if (!exists) {
       await install_FFMPEG();
-    } else console.log("Skipping. Has already installed ffmeg!");
+    } else console.log("Skipping ffmpeg. Has already installed ffmeg!\n");
   } catch (err) {
     console.error("Could not install ffmpeg due: " + err);
   }
 
-  console.log("Finished!");
+  console.log("\nFinished!");
+}
+
+async function fetchRetry(url, fallback_url, tries = 2) {
+  const headers = GITHUB_TOKEN
+    ? { Authorization: `Bearer ${GITHUB_TOKEN}` }
+    : {};
+  function onError(err) {
+    if (--tries <= 0) {
+      if (!fallback_url || url === fallback_url) {
+        throw "Could not fetch from url: " + url;
+      } else {
+        console.error("Fetch failed! Using fallback! URL: " + url);
+        url = fallback_url;
+        tries = 2;
+      }
+    } else {
+      console.warn("Fetch failed! Will try again! URL: " + url);
+    }
+    return wait(200).then(() => fetchRetry(url, fallback_url, tries));
+  }
+  return fetch(url, headers).catch(onError);
+}
+
+function wait(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
 module.exports = init;
