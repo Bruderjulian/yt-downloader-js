55f56418e54b54ba01d6247400d1484c243687d7
BruderJulian <julian@familie-zech.de>
2025-04-14 16:00:57 +0200

    some bugfixes, added Help Menu and polishing

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,43 @@
 const download = require("../src/downloader.js");
 const init = require("../src/init.js");
 
+const help = `        __         __                __             __       
+  __ __/ /________/ /__ _    _____  / /__  ___ ____/ /__ ____
+ / // / __/___/ _  / _ \\ |/|/ / _ \\/ / _ \\/ _ \`/ _  / -_) __/
+ \\_, /\\__/    \\_,_/\\___/__,__/_//_/_/\\___/\\_,_/\\_,_/\\__/_/   
+/___/                                                        
+
+downloads youtube videos from a given url!
+
+Usage:
+  yt-downloader download <url> [path]
+  yt-downloader init [-r] [-t gh_token]
+
+  url   url of the video
+  path  the output path
+  -r    replaces the binaries with new ones
+  -t    specifies the Github Token (not necessary!!)
+
+The downloader must be initialized with 'yt-downloader init', if not already done at installation!
+
+Made by Julizey
+Licensed under MIT
+`;
+
 const argv = process.argv;
-if (argv[2] === "download") {
+for (let i = 0; i < argv.length; i++) argv[i] = argv[i].replace("--", "-");
+const cmd = argv[2];
+if (cmd === "download") {
   download(argv[3], argv[4]);
-} else if (argv[2] === "init") {
-  const gh_token = process.argv[process.argv.indexOf("-t") + 1] || "";
-  const refresh = process.argv.indexOf("-r") !== -1;
+} else if (cmd === "init") {
+  const gh_token = argv[argv.indexOf("-t") + 1] || "";
+  const refresh = argv.indexOf("-r") !== -1;
   init(refresh, gh_token);
+} else if (cmd === "help" || !cmd || argv.indexOf("--help")) {
+  console.log("No command specified! See help:");
+  console.log(help);
+} else if (typeof cmd === "string" && cmd.length > 0) {
+  console.log("Invalid Command! Use 'yt-downloader help' for more info!");
 }
+
 module.exports = { init, download };
--- src/downloader.js
+++ src/downloader.js
@@ -1,19 +1,25 @@
+const { existsSync } = require("fs");
+
 const exec = require("child_process").exec;
 const join = require("path").join;
 
-const ytdlp_path = join(__dirname, "../", "/yt-dlp.exe");
-const ffmpeg_path = join(
-  __dirname,
-  "../",
-  `/ffmpeg${process.platform === "win32" ? ".exe" : ""}`
+const ytdlp_path = escapePath(join(__dirname, "../", "/yt-dlp.exe"));
+const ffmpeg_path = escapePath(
+  join(__dirname, "../", `/ffmpeg${process.platform === "win32" ? ".exe" : ""}`)
 );
 
 async function download(url, output) {
   if (!url) throw new SyntaxError("No Url specified");
+  if (!existsSync(ytdlp_path) || !existsSync(ffmpeg_path)) {
+    return console.error(
+      "Could not find required binaries! To likely resolve the issue run 'yt-downloader init'!"
+    );
+  }
+
   console.log("Downloading Video. May take a while!");
-  const args = `${escape(ytdlp_path)} --ffmpeg-location ${escape(
-    ffmpeg_path
-  )} ${url}${output ? "--output " + output : ""}`;
+  const args = `${ytdlp_path} --ffmpeg-location ${ffmpeg_path} ${url}${
+    output ? "--output " + output : ""
+  }`;
 
   var child = exec(args);
   child.stdout.pipe(process.stdout);
@@ -34,7 +40,7 @@ async function download(url, output) {
   });
 }
 
-function escape(path) {
+function escapePath(path) {
   return path.includes(" ") ? `"${path}"` : path;
 }
 
